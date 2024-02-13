3a8dac44a59fe6c842297f3369eb45033a50863d
Bhavadeep Bhukya <bhavadeepbhukya@Bhavadeeps-MacBook-Air.local>
2024-02-13 17:46:08 +0530

    making integer division

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -16,8 +16,8 @@ class Snake:
 
 class Food:
     def __init__(self):
-        x = random.randint(0,GAME_WIDTH/SPACE_SIZE-1)*SPACE_SIZE
-        y = random.randint(0,GAME_HEIGHT/SPACE_SIZE-1)*SPACE_SIZE
+        x = random.randint(0,GAME_WIDTH//SPACE_SIZE-1)*SPACE_SIZE
+        y = random.randint(0,GAME_HEIGHT//SPACE_SIZE-1)*SPACE_SIZE
 
         self.coordinates = [x,y]
         canvas.create_oval(x,y, x+SPACE_SIZE,y+SPACE_SIZE,fill=FOOD_COLOR,tag="food")
